60e181dbe8085aeddcdb6607b465b7ef6d04d56f
hieungocnguyen <ngnohieu@gmail.com>
2024-01-12 10:46:32 +0700

    feat: Add framer-motion and html-to-image

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,12 @@
 import "./app.css";
 import Lixi from "./components/Lixi";
-import { useEffect, useState } from "preact/hooks";
+import { useCallback, useEffect, useRef, useState } from "preact/hooks";
+import { toPng } from "html-to-image";
 
 export function App() {
   const [step, setStep] = useState(1);
   const [isOpen, setIsOpen] = useState(false);
+  const ref = useRef(null);
 
   if (step === 1) {
     setTimeout(() => {
@@ -16,20 +18,42 @@ export function App() {
     setTimeout(() => setStep(3), 1000);
   }
 
+  const onButtonClick = useCallback(() => {
+    if (ref.current === null) {
+      return;
+    }
+
+    toPng(ref.current, { cacheBust: true })
+      .then((dataUrl) => {
+        const link = document.createElement("a");
+        link.download = "my-image-name.png";
+        link.href = dataUrl;
+        link.click();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [ref]);
+
   return (
     <div className="container">
       <div
         class={`flip-box ${step === 3 ? "appear" : ""}`}
         onClick={() => setIsOpen(true)}
       >
-        <div class={`flip-box-inner ${isOpen ? "openCard" : ""}`}>
+        <div class={`flip-box-inner ${isOpen ? "openCard" : ""}`} ref={ref}>
           <div class="flip-box-front">
             <h2>Tap to open</h2>
           </div>
           <div class="flip-box-back">
-            <h2>...</h2>
+            <h2>quotes random</h2>
           </div>
         </div>
+        {isOpen && (
+          <div className="button-download" onClick={onButtonClick}>
+            DownLoad
+          </div>
+        )}
       </div>
       <div
         className={`lixi ${step == 1 ? "shake" : ""} ${
